1d9c79ebd918e3e4edfe3ceef678c10afa731e2e
UmarMsani <umuhammad220@gmail.com>
2024-03-14 16:28:11 +0000

    update

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -17,5 +17,4 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     def multiplier_function(x: float) -> float:
         return x * multiplier
-    
     return multiplier_function
