a59594e2597d49f6d9e22575178bcc481521595e
UmarMsani <umuhammad220@gmail.com>
2024-03-20 02:17:00 +0000

    Implemented async_generator coroutine

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Async Generator
+This module provides a coroutine called async_generator that
+yields random numbers asynchronously.
+"""
+
+import asyncio
+import random
+
+
+async def async_generator() -> asyncio.Queue:
+    """
+    Coroutine that yields random numbers asynchronously.
+
+    Yields:
+        int: A random number between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
